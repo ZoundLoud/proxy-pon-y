3119c27adb5558b59fcfaefc03a2a12abbb371f6
Yvonne <>
2019-02-19 19:32:29 -0800

    Add 2 teammate micro services

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require ('express');
-const path = require('path')
+const path = require('path');
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.use(express.static(path.join(__dirname , '../client')));
+app.use(express.static(path.join(__dirname, '../client')));
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
 
