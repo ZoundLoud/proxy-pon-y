2c005ebc6a676210a90b76ee3c33891656297256
Yvonne <>
2019-02-21 19:58:22 -0800

    Add one deployed module

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const path = require('path');
 const app = express();
+const morgan = require('morgan');
+
 
 const port = process.env.PORT || 4000;
-
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(morgan());
+app.use('/song/:songId', express.static(path.resolve(__dirname, '../client')));
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
 
